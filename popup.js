4ec1daae04a1be8ce9e4824e51d512a1122e33ba
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2022-05-14 12:07:47 +0530

    add

--- popup.js
+++ popup.js
@@ -47,6 +47,8 @@ function buy() {
   buyBox.style.opacity = "1";
   sellBox.style.opacity = "0.5";
   document.getElementById('btn').style.backgroundColor = "#ffcc00";
+  document.getElementById('buy-popup').style.color = "#ffcc00";
+  document.getElementById('sell-popup').style.color = "";
 };
 function sell() {
   var sellBox = document.getElementById('sell-popup');
@@ -57,126 +59,159 @@ function sell() {
   sellBox.style.opacity = "1";
   buyBox.style.opacity = "0.5";
   document.getElementById('btn').style.backgroundColor = "#ffcc00";
+  document.getElementById('sell-popup').style.color = "#ffcc00";
+    document.getElementById('buy-popup').style.color = "";
 }
-// step 2 condition
-document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
+
+// second step 
+
+document.getElementById('second1').addEventListener('click', second1);
+document.getElementById('btn2').disabled = true;
+
 function second1() {
-  document.getElementById('second1').style.border = "1px solid #ffc107";
-  document.getElementById('second1').style.opacity = "1";
-  document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond1() {
-  document.getElementById('second1').style.border = "";
-  document.getElementById('second1').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second1').classList.toggle('col-0');
+  let a = document.getElementById('second1').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second2').addEventListener('click', second2);
+document.getElementById('btn2').disabled = true;
+
 function second2() {
-  document.getElementById('second2').style.border = "1px solid #ffc107";
-  document.getElementById('second2').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond2() {
-  document.getElementById('second2').style.border = "";
-  document.getElementById('second2').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second2').classList.toggle('col-0');
+  let a = document.getElementById('second2').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second3').addEventListener('click', second3);
+document.getElementById('btn2').disabled = true;
+
 function second3() {
-  document.getElementById('second3').style.border = "1px solid #ffc107";
-  document.getElementById('second3').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond3() {
-  document.getElementById('second3').style.border = "";
-  document.getElementById('second3').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second3').classList.toggle('col-0');
+  let a = document.getElementById('second3').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second4').addEventListener('click', second4);
+document.getElementById('btn2').disabled = true;
+
 function second4() {
-  document.getElementById('second4').style.border = "1px solid #ffc107";
-  document.getElementById('second4').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond4() {
-  document.getElementById('second4').style.border = "";
-  document.getElementById('second4').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second4').classList.toggle('col-0');
+  let a = document.getElementById('second4').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second5').addEventListener('click', second5);
+document.getElementById('btn2').disabled = true;
+
 function second5() {
-  document.getElementById('second5').style.border = "1px solid #ffc107";
-  document.getElementById('second5').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond5() {
-  document.getElementById('second5').style.border = "";
-  document.getElementById('second5').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second5').classList.toggle('col-0');
+  let a = document.getElementById('second5').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second6').addEventListener('click', second6);
+document.getElementById('btn2').disabled = true;
+
 function second6() {
-  document.getElementById('second6').style.border = "1px solid #ffc107";
-  document.getElementById('second6').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond6() {
-  document.getElementById('second6').style.border = "";
-  document.getElementById('second6').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second6').classList.toggle('col-0');
+  let a = document.getElementById('second6').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second7').addEventListener('click', second7);
+document.getElementById('btn2').disabled = true;
+
 function second7() {
-  document.getElementById('second7').style.border = "1px solid #ffc107";
-  document.getElementById('second7').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond7() {
-  document.getElementById('second7').style.border = "";
-  document.getElementById('second7').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second7').classList.toggle('col-0');
+  let a = document.getElementById('second7').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second8').addEventListener('click', second8);
+document.getElementById('btn2').disabled = true;
+
 function second8() {
-  document.getElementById('second8').style.border = "1px solid #ffc107";
-  document.getElementById('second8').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond8() {
-  document.getElementById('second8').style.border = "";
-  document.getElementById('second8').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second8').classList.toggle('col-0');
+  let a = document.getElementById('second8').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second9').addEventListener('click', second9);
+document.getElementById('btn2').disabled = true;
+
 function second9() {
-  document.getElementById('second9').style.border = "1px solid #ffc107";
-  document.getElementById('second9').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor = "#ffcc00";
-};
-function dsecond9() {
-  document.getElementById('second9').style.border = "";
-  document.getElementById('second9').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second9').classList.toggle('col-0');
+  let a = document.getElementById('second9').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
+document.getElementById('second10').addEventListener('click', second10);
+document.getElementById('btn2').disabled = true;
+
 function second10() {
-  document.getElementById('second10').style.border = "1px solid #ffc107";
-  document.getElementById('second10').style.opacity = "1";
-    document.getElementById('btn2').removeAttribute('disabled');
-  document.getElementById('btn2').style.backgroundColor="#ffcc00"
-};
-function dsecond10() {
-  document.getElementById('second10').style.border = "";
-  document.getElementById('second10').style.opacity = "0.5";
-  document.getElementById('btn2').disabled = "true";
-  document.getElementById('btn2').style.backgroundColor = "rgb(206, 212, 218)";
-};
+  document.getElementById('second10').classList.toggle('col-0');
+  let a = document.getElementById('second10').classList.contains('col-0');
+  var bt = document.getElementById('btn2');
+  bt.disabled = !a
+  if (bt.disabled = !a) {
+    bt.style.backgroundColor = "rgb(206, 212, 218)";
+  } else {
+    bt.style.backgroundColor = "yellow";
+  }
+
+}
