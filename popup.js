3607c4ce47fc06fbe5888a7e4e92bf5320e08cc5
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-05-24 17:32:52 +0530

    add

--- popup.js
+++ popup.js
@@ -9,6 +9,7 @@
       document.getElementById('text2').innerHTML="2";
       document.getElementById('welcome').innerHTML="Help us improve your experience with us in future.";
       document.getElementById('subDetail').innerHTML = "Select the categories that are relevant to you:";
+      document.getElementById('subDetail').style.marginLeft="45px";
       document.getElementById('step1').style.display = "none";
       document.getElementById('step2').style.display = "block";
     document.getElementById('popup').style.margin = "2%  3%";
