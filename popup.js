452e8276068a3e06f59b021f6f5902554896227d
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-06-01 22:06:18 +0530

    popup working

--- popup.js
+++ popup.js
@@ -1,41 +1,15 @@
-// POP UP FORM
-    // document.body.style.position = "fixed";
-    function PopUp ()
-    {
-      document.getElementById('popupBox').style.display = "none";
-      document.body.style.position = "static";
-   };
-  function step2(){
-      document.getElementById('text2').innerHTML="2";
-      document.getElementById('welcome').innerHTML="Help us improve your experience with us in future.";
-      document.getElementById('subDetail').innerHTML = "Select the categories that are relevant to you:";
-      document.getElementById('subDetail').style.marginLeft="45px";
-      document.getElementById('step1').style.display = "none";
-      document.getElementById('step2').style.display = "block";
-    document.getElementById('popup').style.margin = "2%  3%";
-      // document.getElementById('sm-pop').style.marginLeft = "-23%";
-};
-    
-      const counters = document.querySelectorAll(".count");
-const speed = 200;
 
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(+counter.getAttribute("data-target"));
-    const count = parseInt(+counter.innerText);
-    const increment = Math.trunc(target / speed);
-    console.log(increment);
 
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      count.innerText = target;
-    }
-  };
-  updateCount();
-});
+document.getElementById("btnl").addEventListener("click" , abc);
 
+function abc(){
+  document.getElementById('Close').style.display = "none"
+}
+document.getElementById("btn").addEventListener("click" , xyz);
+
+function xyz(){
+   document.getElementById('Close').style.display = "none"
+}
 // pop up form step one
 var nextB = document.getElementById('btn').style.backgroundColor = "rgb(206, 212, 218)";
 // document.getElementById('step1').style.opacity = "0.5";
